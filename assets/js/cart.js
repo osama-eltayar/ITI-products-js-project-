a3f6c07df1f07fc0dd276f96f47f39d95d0d9fde
HagarMohamedGamal <hagar.mohamed.gamal@gmail.com>
2020-03-09 17:30:45 +0200

    organizing code

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -3,6 +3,9 @@ $((ev)=>{
     let countItems=0;
     let price=0;
     const elem = document.querySelector('.items');
+
+    //fires when all the products are fetched from the api and listed in the home page
+    //to view which product was checked before and stored in the localStorage
     elem.addEventListener('getAllBtns', (e) => {
         $(".items button[id^='HT-']").each((i,el)=>{
             idIndex = $(el).attr("id");
@@ -12,7 +15,7 @@ $((ev)=>{
         });
     }, false);
 
-
+    //fires when all the products are fetched from the api and listed in the home page
     elem.addEventListener('getAllCheckedData', (e) => {
             Object.values(localStorage).forEach(element => {
                 element = JSON.parse(element);
@@ -26,6 +29,7 @@ $((ev)=>{
             });
     }, false);
     
+    //fires when check button is pressed
     $(".items").on("click","button[id^='HT-']" ,(ev)=>{
         idVal = $(ev.currentTarget).attr("id");
         console.log("clicked", idVal);
@@ -35,30 +39,26 @@ $((ev)=>{
 
 
     function setStorage(index, num){
+        prodPrice = $(`.price.${index}`).text();
+        price = $("#price").text();
         if(localStorage.getItem(index))
         {
+            //remove items from localStorage
             console.log("exists");
-            
-            prodPrice = $(`.price.${index}`).text();
-            price = $("#price").text();
             price = (+price) - (+prodPrice)
             countItems-=1;
-            $("#price").text(price);
-            $(".num").text(countItems);
             localStorage.removeItem(index)
         }
         else{
+            //add items to localStorage
             console.log("not exists");
-            prodPrice = $(`.price.${index}`).text();
-            price = $("#price").text();
             price = (+price) + (+prodPrice)
             countItems+=1;
-            $("#price").text(price);
-            $(".num").text(countItems);
-
             item = {"id":num, "q":1, "price":prodPrice};
             localStorage.setItem(index,JSON.stringify(item));
         }
+        $("#price").text(price);
+        $(".num").text(countItems);
     }
 
     function getStorage(index){
--- assets/js/cartPage.js
+++ assets/js/cartPage.js
@@ -1,12 +1,30 @@
+
+cartProduct = getLocalStorageData();
+cartProduct.forEach((e)=>{
+    getProductData(e);
+    
+})
+
 const elem = document.querySelector('table');
+
 elem.addEventListener('getAllCart', (e) => {
     q = JSON.parse(localStorage.getItem(e.detail.id)).q;
-    $("table tbody").append("<tr>"+
-    "<td width='100px'><img class='img-thumbnail my-2' src='"+e.detail.image+
-    "' width='60px' height='100px'></td><td width='140px'>"+e.detail.name+"</td>"+
-    "<td class='"+e.detail.id+"'>"+e.detail.price+"</td>"+
-    "<td><input class='"+e.detail.id+"' type='text' name='quantity' value='"+q+"'></td>"+
-    "<td class='"+e.detail.id+" total' >"+e.detail.price*q+"</td>"+
+    $("table tbody").append(
+    "<tr>"+
+        "<td width='100px'>"+
+            "<img class='img-thumbnail my-2' src='"+e.detail.image+"' width='60px' height='100px'>"+
+        "</td>"+
+
+        "<td width='140px'>"+e.detail.name+"</td>"+
+
+        "<td class='"+e.detail.id+"'>"+e.detail.price+"</td>"+
+
+        "<td>"+
+            "<input class='"+e.detail.id+"' type='text' name='quantity' value='"+q+"'>"+
+        "</td>"+
+
+        "<td class='"+e.detail.id+" total' >"+e.detail.price*q+"</td>"+
+
     "</tr>");
     getTotalPrice();
     
@@ -40,6 +58,7 @@ $("table").on("click","input" , function() {
     }).change();
 });
 
+//update number of quantities in the localStorage
 function updateQuantity(cls, q){
     quantStorage = JSON.parse(localStorage.getItem(cls))
     quantStorage.q = q;
@@ -47,13 +66,7 @@ function updateQuantity(cls, q){
 }
 
 
-cartProduct = getLocalStorageData();
-cartProduct.forEach((e)=>{
-    getProductData(e);
-    
-})
-
-
+//do a validation check on the quantity entered by the customer
 function checkQuantityValid(quant){
     if(isNaN(quant)){
         return false;
@@ -65,15 +78,16 @@ function getLocalStorageData(){
     arr=[]
     for (i in localStorage) {
         if (localStorage.hasOwnProperty(i)) {
-        if (i.match(/^HT-[0-9]+/)) {
-            value = JSON.parse(localStorage.getItem(i));
-            arr.push(value);
-        }
+            if (i.match(/^HT-[0-9]+/)) {
+                value = JSON.parse(localStorage.getItem(i));
+                arr.push(value);
+            }
         }
     }
     return arr;
 }
-var count = 0;
+
+//get checked product's details to display it on the cart page
 function getProductData(idIndex){
     
     let myFirstPromise = new Promise((resolve, reject) => {
@@ -96,7 +110,6 @@ function getProductData(idIndex){
         let price = item.Price;
         let image = item.ProductPicUrl;
         let quantity = idIndex.q;
-        count++;
         details = 
             {"id":idIndex.id, "name": name, "price": price, "image": image, "quantity":quantity};
         
