05107e3f5a0944fd628eaca36f00f9239b29e8c3
SafaaMohamedGamal <safaa.mohamed.gamal@gmail.com>
2020-03-12 23:14:24 +0200

    pagination with filters

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,146 +7,62 @@ $(function()
     let url= "?limit="+limit+"&page="+page
     let dataEvent = new Event('getAllCheckedData');
     const elem = document.querySelector('.items');
-    const cate = document.querySelector("#catnav");
-    const supname = document.querySelector("#supnav");
-    cate.addEventListener('click',chooseCategory);
-    let category ;
-    let catFlag=false;
-    supname.addEventListener('click', chooseSupplier);
-    let supplier;
-    let supFlag = false;
-    elem.dispatchEvent(dataEvent);
-
+    $(".page1").empty();
+    $("<button></button>")
+        .addClass("btn page-link")
+        .text(page)
+        .appendTo(".page1")
+        .css({background:'orange'})
     $(".pagePrev").hide();
-    $(".page-link:eq(0)").text(page).css({background:'orange'})
-
-    let p= new products;
+    let p= new products(limit);
     p.useProductPage(url).then((ev)=>{
         let dataEvent = new Event('getAllCheckedData');
         const elem = document.querySelector('.items');
         elem.dispatchEvent(dataEvent);
     });
 
+    $(".page-link").on('click', (e)=>{
+        page = parseInt(e.target.innerText);
+        p.pageNavigation(page, catFlag, supFlag, category, supplier);
+    });
+
+    $(".pagePrev").on('click',()=>{
+        page -=1;
+        p.pageNavigation(page, catFlag, supFlag, category, supplier);
+    })
+    $(".pageNext").on('click',()=>{
+        page +=1;
+        p.pageNavigation(page, catFlag, supFlag, category, supplier);
+    })
+
     /*************************************************************************/
     $("#searchBox").keypress(function(event){
         var keycode = (event.keyCode ? event.keyCode : event.which);
         if(keycode == '13'){
-          //  console.log($(this).val());
-            //alert('You pressed a "enter" key in textbox');
              url= "?q="+$(this).val();
-            console.log(url);
-          //  alert('You pressed a "enter" key in textbox');
-            // p= new products;
+            // console.log(url);
             p.useProductPage(url)
-            // elem.dispatchEvent(dataEvent);
         }
-      //  event.stopPropagation();
-    });
-/*************************************************************************/
-
-    $(".page-link").on('click', (e)=>{
-        page = parseInt(e.target.innerText);
-        if (page != 1) {
-            $(".pagePrev").show()
-            if (page<=p.getPagesNum()-2) {
-                $(".page-link:eq(0)").text(page).css({background:'orange'})
-                $(".page-link:eq(1)").text(page+1).css({background:'white'})
-                $(".page-link:eq(2)").text(page+2).css({background:'white'})
-            }
-            else if (page<=p.getPagesNum()-1) {
-                $(".page-link:eq(0)").text(page-1).css({background:'white'})
-                $(".page-link:eq(1)").text(page).css({background:'orange'})
-                $(".page-link:eq(2)").text(page+1).css({background:'white'})
-            }
-            else if (page<=p.getPagesNum()) {
-                $(".page-link:eq(0)").text(page-2).css({background:'white'})
-                $(".page-link:eq(1)").text(page-1).css({background:'white'})
-                $(".page-link:eq(2)").text(page).css({background:'orange'})
-                $(".pageNext").hide()
-            }
-        }
-        
-        console.log(e.target.innerText);
-        if (catFlag)
-            url = "?limit=" + limit + "&page=" + page + "&category=" + category;
-        else if (supFlag)
-            url = "?limit=" + limit + "&page=" + page + "&supplier=" + supplier;
-        else
-            url = "?limit=" + limit + "&page=" + page;
-        console.log("page    " + url);
-        p.useProductPage(url);
-
     });
 
-    $(".pagePrev").on('click',()=>{
-        if (page==1) {
-            console.log("end");
-        }
-        else
-        {
-            $(".pageNext").show()
-            page = page-1
-            $(".page-link:eq(0)").text(page).css({background:'orange'})
-            $(".page-link:eq(1)").text(page+1).css({background:'white'})
-            $(".page-link:eq(2)").text(page+2).css({background:'white'})
-            if (page==1) {
-                $(".pagePrev").hide();
-            }
-            console.log(page);
-            if (catFlag)
-                url = "?limit=" + limit + "&page=" + page + "&category=" + category;
-            else if (supFlag)
-                url = "?limit=" + limit + "&page=" + page + "&supplier=" + supplier;
-            else
-                url = "?limit=" + limit + "&page=" + page;
-            p.useProductPage(url)
-        }
-    })
-    $(".pageNext").on('click',()=>{
-        // https://www.sitepoint.com/get-url-parameters-with-javascript/
-        // url = "contact.html?id=1"
-        // window.location.href = url;
-        // const queryString = window.location.search;
-        // console.log(queryString);
-        // const urlParams = new URLSearchParams(queryString);
-        // const id = urlParams.get('id')
-        // console.log(id);
-        if (page == p.getPagesNum()) {
-            console.log("end");
-        }
-        else
-        {
-            $(".pagePrev").show()
-            console.log("=="+page);
-            page = page+1
-            if (page <= p.getPagesNum()-2) {
-                $(".page-link:eq(0)").text(page).css({background:'orange'})
-                $(".page-link:eq(1)").text(page+1).css({background:'white'})
-                $(".page-link:eq(2)").text(page+2).css({background:'white'})
-            }
-            else if (page<=p.getPagesNum()-1) {
-                $(".page-link:eq(0)").text(page-1).css({background:'white'})
-                $(".page-link:eq(1)").text(page).css({background:'orange'})
-                $(".page-link:eq(2)").text(page+1).css({background:'white'})
-            }
-            else if (page<=p.getPagesNum()) {
-                $(".page-link:eq(0)").text(page-2).css({background:'white'})
-                $(".page-link:eq(1)").text(page-1).css({background:'white'})
-                $(".page-link:eq(2)").text(page).css({background:'orange'})
-                $(".pageNext").hide()
-            }
-            console.log(page);
-            if (catFlag)
-                url = "?limit=" + limit + "&page=" + page + "&category=" + category;
-            else if(supFlag)
-                url = "?limit=" + limit + "&page=" + page + "&supplier=" + supplier;
-            else
-                url = "?limit=" + limit + "&page=" + page;    
-            p.useProductPage(url)
-        }
-    })
     // *********************************************************************
     // list categories  & suppliers
+
+    let category ;
+    let catFlag=false;
+    let supplier;
+    let supFlag = false;
+    const cate = document.querySelector("#catnav");
+    const supname = document.querySelector("#supnav");
+    cate.addEventListener('click',(ev)=>{
+        page = 1;
+        chooseCategory(ev, page);
+    });
+    supname.addEventListener('click', (ev)=>{
+        page = 1;
+        chooseSupplier(ev, page);
+    });
+
     $.ajax({
         method: "GET",
         url: "https://afternoon-falls-30227.herokuapp.com/api/v1/products-stats/"
@@ -154,54 +70,43 @@ $(function()
     .done(function (response) {
         let cats = Object.keys(response.data.Groups.Category)
         let supps = Object.keys(response.data.Groups.SupplierName);
-        console.log(supps);
         console.log("--------------------");
         cats.forEach((value)=>{
-            console.log(value);
             $("<a class=dropdown-item >"+value+"</a>").appendTo("#catnav");
         })
         supps.forEach((na) => {
-            console.log(na+"    item");
             $("<a class=dropdown-item >" + na + "</a>").appendTo("#supnav");
         })
     });
     // **************************************
-    function chooseCategory(ev)
+    function chooseCategory(ev,page)
     {
         category = ev.target.text;
         console.log(category);
         if (category != "All")
         {
             catFlag = true;
-            page = 1;
-            url = "?limit=" + limit + "&page=" + page + "&category=" + category;
         }
         else
         {
             catFlag = false;
-            page = 1;
-            url = "?limit=" + limit + "&page=" + page ;
         }
-        
-        p.useProductPage(url)
+        p.pageNavigation(page, catFlag, supFlag, category, supplier)
     }
-// **********************************************************************
-    function chooseSupplier(ev) {
+    
+    function chooseSupplier(ev, page) 
+    {
         supplier = ev.target.text;
         console.log(supplier);
-        if (supplier != "All") {
+        if (supplier != "All") 
+        {
             supFlag = true;
-            catFlag = false;
-            page = 1;
-            url = "?limit=" + limit + "&page=" + page + "&supplier=" + supplier;
         }
-        else {
+        else 
+        {
             supFlag = false;
-            page = 1;
-            url = "?limit=" + limit + "&page=" + page;
         }
-
-        p.useProductPage(url)
+        p.pageNavigation(page, catFlag, supFlag, category, supplier)
     }
     // *******************************************************************
     
@@ -209,25 +114,10 @@ $(function()
 
 class products
 {
-    constructor()
+    constructor(limit)
     {
         this.total_pages=0;
-    }
-    async getProductPage(urlPage)
-    {
-        return await new Promise((resolve, reject)=>{
-            $.ajax({
-                url:"https://afternoon-falls-30227.herokuapp.com/api/v1/products"+urlPage, //path or url
-                success:function(response)
-                {
-                    resolve(response)
-                } ,
-                error:function()
-                {
-                    reject("error xxx")
-                }
-            });
-        })
+        this.limit=limit;
     }
 
     async useProductPage(url)
@@ -251,28 +141,25 @@ class products
         })
         let msg = await promise;
         this.reListProducts();
-        // return this.getProductPage(url).then(msg=>{
-            this.total_pages = msg.total_pages
-            console.log("total pages:",this.total_pages);
-            let items = msg.data;
-            items.forEach(item => {
-                let id = item.ProductId;
-                let name = item.Name;
-                let price = item.Price;
-                let image = item.ProductPicUrl
-                let length = items.length
-                this.listProducts(id, name, price, image)
-            });
-            const elem = document.querySelector('.items');
-            let ColorEvent = new Event('getAllBtns');
-            elem.dispatchEvent(ColorEvent);
-            $(".details").on("click",showDetails)
-
-            $('.mask').on('click', function(){
-                $('.mask').fadeOut();
-                $('#quick-view-pop-up').fadeOut();
-            });
-        // });
+        this.total_pages = msg.total_pages
+        console.log("total pages:",this.total_pages);
+        let items = msg.data;
+        items.forEach(item => {
+            let id = item.ProductId;
+            let name = item.Name;
+            let price = item.Price;
+            let image = item.ProductPicUrl
+            let length = items.length
+            this.listProducts(id, name, price, image)
+        });
+        const elem = document.querySelector('.items');
+        let ColorEvent = new Event('getAllBtns');
+        elem.dispatchEvent(ColorEvent);
+        $(".details").on("click",showDetails)
+        $('.mask').on('click', function(){
+            $('.mask').fadeOut();
+            $('#quick-view-pop-up').fadeOut();
+        });
     }
 
 
@@ -321,37 +208,91 @@ class products
     {
         return this.total_pages;
     }
+
+    pageNavigation(page, catFlag, supFlag, category, supplier)
+    {
+        let url = "?limit=" + this.limit + "&page=" + page ;  
+        if (catFlag)
+            url += "&category=" + category;
+        if(supFlag)
+            url += "&supplier=" + supplier;
+        this.useProductPage(url).then((ev)=>{
+            console.log("pages:  "+this.total_pages+",    page:  "+page);
+            if(this.total_pages>0)
+            {
+                if (page == this.total_pages && this.total_pages==1) {
+                    $(".pageNext").hide();
+                    $(".pagePrev").hide();
+                    $(".page1").empty();
+                    console.log("end");
+                }
+                else 
+                {
+                    if (page == this.total_pages && this.total_pages>1) {
+                    $(".pageNext").hide();
+                    $(".pagePrev").show();
+                    console.log("end");
+                    }
+                    else if (page == 1 && this.total_pages>1)
+                    {
+                        $(".pageNext").show();
+                        $(".pagePrev").hide();
+                    }
+                    else
+                    {
+                        $(".pageNext").show()
+                        $(".pagePrev").show()
+                    }
+                    $(".page1").empty();
+                    $("<button></button>")
+                        .addClass("btn page-link")
+                        .text(page)
+                        .appendTo(".page1")
+                        .css({background:'orange'}) 
+                }
+            }
+            else
+            {
+                $("<div>No products here!..</div>").appendTo(".items")
+                $(".pageNext").hide();
+                $(".pagePrev").hide();
+            }
+        });
+    
+    }
+
 }
 
-function showDetails(){
+function showDetails()
+{
     console.log("clicked");
 
-  console.log($(this).parent().siblings(".checkout").children().attr("id"));
-  let itemId=$(this).parent().siblings(".checkout").children().attr("id");
-  $.getJSON("https://afternoon-falls-30227.herokuapp.com/api/v1/products/"+itemId, function(result){
-  //console.log(result.data.SupplierName);
-      $("#myImage").attr("src",result.data.ProductPicUrl);
-      $("#myDetailedImage").attr("src",result.data.ProductPicUrl);
-      $(".product-name").text(result.data.Name);
-      $(".description").text(result.data.Description);
-      $(".product-price").text(result.data.Price);
+    console.log($(this).parent().siblings(".checkout").children().attr("id"));
+    let itemId=$(this).parent().siblings(".checkout").children().attr("id");
+    $.getJSON("https://afternoon-falls-30227.herokuapp.com/api/v1/products/"+itemId, function(result){
+    //console.log(result.data.SupplierName);
+        $("#myImage").attr("src",result.data.ProductPicUrl);
+        $("#myDetailedImage").attr("src",result.data.ProductPicUrl);
+        $(".product-name").text(result.data.Name);
+        $(".description").text(result.data.Description);
+        $(".product-price").text(result.data.Price);
 
-      $(".status").text(result.data.Status);
+        $(".status").text(result.data.Status);
 
-      $(".add-to-bag").text("Add to Cart")
-      if(localStorage.hasOwnProperty(itemId))
-      {
+        $(".add-to-bag").text("Add to Cart")
+        if(localStorage.hasOwnProperty(itemId))
+        {
         $(".add-to-bag").text("Remove from Cart")
-      }
-  });
+        }
+    });
 
-  $(".add-to-bag").unbind().on("click", (e)=>{
-      console.log("itemId");
-      let clickEvent = new CustomEvent("clickCart", {'detail': {"id":itemId}});
-      const elem = document.querySelector('.items');
-      elem.dispatchEvent(clickEvent);
-  })
-  $('#quick-view-pop-up').fadeToggle();
-  $('#quick-view-pop-up').css({"top":"34px", "left":"314px"});
-  $('.mask').fadeToggle();
+    $(".add-to-bag").unbind().on("click", (e)=>{
+        console.log("itemId");
+        let clickEvent = new CustomEvent("clickCart", {'detail': {"id":itemId}});
+        const elem = document.querySelector('.items');
+        elem.dispatchEvent(clickEvent);
+    })
+    $('#quick-view-pop-up').fadeToggle();
+    $('#quick-view-pop-up').css({"top":"34px", "left":"314px"});
+    $('.mask').fadeToggle();
 }
